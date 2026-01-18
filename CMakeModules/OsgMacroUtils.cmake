madsbuvi/OpenSceneGraph

commit 7195e5938b0554c8f000e4b4918388786704c13d

Always install PDB files for all targets

CMakeModules/OsgMacroUtils.cmake
@@ -349,13 +349,7 @@ MACRO(SETUP_PLUGIN PLUGIN_NAME)
             ARCHIVE DESTINATION lib/${OSG_PLUGINS} COMPONENT libopenscenegraph-dev
             LIBRARY DESTINATION bin/${OSG_PLUGINS} COMPONENT ${PACKAGE_COMPONENT})
         IF(MSVC AND DYNAMIC_OPENSCENEGRAPH)
-	    IF( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
-		INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_TARGETNAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION bin/${OSG_PLUGINS} COMPONENT ${PACKAGE_COMPONENT} CONFIGURATIONS RelWithDebInfo)
-		INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${TARGET_TARGETNAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION bin/${OSG_PLUGINS} COMPONENT ${PACKAGE_COMPONENT} CONFIGURATIONS Debug)
-	    ELSE( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
-                INSTALL(FILES ${OUTPUT_BINDIR}/${OSG_PLUGINS}/${TARGET_TARGETNAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION bin/${OSG_PLUGINS} COMPONENT ${PACKAGE_COMPONENT} CONFIGURATIONS RelWithDebInfo)
-                INSTALL(FILES ${OUTPUT_BINDIR}/${OSG_PLUGINS}/${TARGET_TARGETNAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION bin/${OSG_PLUGINS} COMPONENT ${PACKAGE_COMPONENT} CONFIGURATIONS Debug)
-	    ENDIF( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
+            INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_TARGETNAME}> DESTINATION bin/${OSG_PLUGINS} COMPONENT ${PACKAGE_COMPONENT})
         ENDIF(MSVC AND DYNAMIC_OPENSCENEGRAPH)
     ELSE(WIN32)
         INSTALL(TARGETS ${TARGET_TARGETNAME}
@@ -463,8 +457,7 @@ MACRO(SETUP_APPLICATION APPLICATION_NAME)
         ELSE(APPLE)
             INSTALL(TARGETS ${TARGET_TARGETNAME} RUNTIME DESTINATION bin COMPONENT openscenegraph  )
             IF(MSVC)
-		INSTALL(FILES ${OUTPUT_BINDIR}/${TARGET_NAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION bin COMPONENT openscenegraph CONFIGURATIONS RelWithDebInfo)
-		INSTALL(FILES ${OUTPUT_BINDIR}/${TARGET_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION bin COMPONENT openscenegraph CONFIGURATIONS Debug)
+                INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION bin COMPONENT openscenegraph)
             ENDIF(MSVC)
         ENDIF(APPLE)
 
@@ -496,8 +489,7 @@ MACRO(SETUP_EXAMPLE EXAMPLE_NAME)
         ELSE(APPLE)
             INSTALL(TARGETS ${TARGET_TARGETNAME} RUNTIME DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples )
             IF(MSVC)
-		INSTALL(FILES ${OUTPUT_BINDIR}/${TARGET_NAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples CONFIGURATIONS RelWithDebInfo)
-		INSTALL(FILES ${OUTPUT_BINDIR}/${TARGET_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples CONFIGURATIONS Debug)
+                INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples)
             ENDIF(MSVC)
         ENDIF(APPLE)
 

CMakeLists.txt
@@ -408,6 +408,10 @@ IF(WIN32 AND NOT ANDROID)
             SET(CMAKE_SHARED_LINKER_FLAGS_DEBUG "/debug /INCREMENTAL:NO")
             SET(CMAKE_EXE_LINKER_FLAGS_DEBUG "/debug /INCREMENTAL:NO")
         ENDIF(NOT OSG_MSVC_DEBUG_INCREMENTAL_LINK)
+
+        SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} /DEBUG")
+        SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} /DEBUG")
+        SET(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} /DEBUG")
     ENDIF(MSVC)
 ENDIF(WIN32 AND NOT ANDROID)
 

CMakeModules/ModuleInstall.cmake
@@ -33,18 +33,7 @@ INSTALL(
 )
 
 IF(MSVC AND DYNAMIC_OPENSCENEGRAPH)
-    GET_TARGET_PROPERTY(PREFIX ${LIB_NAME} PREFIX)
-    IF("${PREFIX}" STREQUAL PREFIX-NOTFOUND) # Fix for PREFIX-NOTFOUND left in file names
-	SET(PREFIX "")
-    ENDIF()
-    IF ( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
-	INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PREFIX}${LIB_NAME}${CMAKE_RELEASE_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenscenegraph CONFIGURATIONS Release)
-	INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PREFIX}${LIB_NAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenscenegraph CONFIGURATIONS RelWithDebInfo)
-	INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PREFIX}${LIB_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenscenegraph CONFIGURATIONS Debug)
-    ELSE ( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
-	INSTALL(FILES ${OUTPUT_BINDIR}/${PREFIX}${LIB_NAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenscenegraph CONFIGURATIONS RelWithDebInfo)
-	INSTALL(FILES ${OUTPUT_BINDIR}/${PREFIX}${LIB_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenscenegraph CONFIGURATIONS Debug)
-    ENDIF ( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
+    INSTALL(FILES $<TARGET_PDB_FILE:${LIB_NAME}> DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads)
 ENDIF(MSVC AND DYNAMIC_OPENSCENEGRAPH)
 
 IF(NOT OSG_COMPILE_FRAMEWORKS)

src/OpenThreads/win32/CMakeLists.txt
@@ -58,18 +58,7 @@ INSTALL(
 )
 
 IF(MSVC AND DYNAMIC_OPENSCENEGRAPH)
-    GET_TARGET_PROPERTY(PREFIX ${LIB_NAME} PREFIX)
-    IF("${PREFIX}" STREQUAL PREFIX-NOTFOUND) # Fix for PREFIX-NOTFOUND left in file names
-	SET(PREFIX "")
-    ENDIF()
-    IF( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
-	INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PREFIX}${LIB_NAME}${CMAKE_RELEASE_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads CONFIGURATIONS Release)
-	INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PREFIX}${LIB_NAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads CONFIGURATIONS RelWithDebInfo)
-	INSTALL(FILES ${CMAKE_CURRENT_BINARY_DIR}/${PREFIX}${LIB_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads CONFIGURATIONS Debug)
-    ELSE( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
-	INSTALL(FILES ${OUTPUT_BINDIR}/${PREFIX}${LIB_NAME}${CMAKE_RELWITHDEBINFO_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads CONFIGURATIONS RelWithDebInfo)
-	INSTALL(FILES ${OUTPUT_BINDIR}/${PREFIX}${LIB_NAME}${CMAKE_DEBUG_POSTFIX}.pdb DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads CONFIGURATIONS Debug)
-    ENDIF( ${CMAKE_GENERATOR} STREQUAL "Ninja" )
+    INSTALL(FILES $<TARGET_PDB_FILE:${LIB_NAME}> DESTINATION ${INSTALL_BINDIR} COMPONENT libopenthreads)
 ENDIF(MSVC AND DYNAMIC_OPENSCENEGRAPH)
 
 #commented out# INCLUDE(ModuleInstall OPTIONAL)

elsid/osg

commit 537e5fe70747a27e0a4b3da44e2ba11fe3e63ee3

Fix PDB regression
Introduced by 7195e5938b0554c8f000e4b4918388786704c13d

CMakeModules/OsgMacroUtils.cmake
@@ -457,7 +457,7 @@ MACRO(SETUP_APPLICATION APPLICATION_NAME)
         ELSE(APPLE)
             INSTALL(TARGETS ${TARGET_TARGETNAME} RUNTIME DESTINATION bin COMPONENT openscenegraph  )
             IF(MSVC)
-                INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION bin COMPONENT openscenegraph)
+                INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_TARGETNAME}> DESTINATION bin COMPONENT openscenegraph)
             ENDIF(MSVC)
         ENDIF(APPLE)
 
@@ -489,7 +489,7 @@ MACRO(SETUP_EXAMPLE EXAMPLE_NAME)
         ELSE(APPLE)
             INSTALL(TARGETS ${TARGET_TARGETNAME} RUNTIME DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples )
             IF(MSVC)
-                INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_NAME}> DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples)
+                INSTALL(FILES $<TARGET_PDB_FILE:${TARGET_TARGETNAME}> DESTINATION share/OpenSceneGraph/bin COMPONENT openscenegraph-examples)
             ENDIF(MSVC)
         ENDIF(APPLE)
 